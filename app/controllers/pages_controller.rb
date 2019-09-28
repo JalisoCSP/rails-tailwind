30aa34856d938f1f77fc38011bd35b840a744b77
Craig Petterson <craig@calajo.co.uk>
2019-09-28 18:10:23 +0100

    Add generic terms and policy from getterms.io

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,22 @@
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!
 
+  before_action :set_application_name
+
   def splash
+  end
+
+  def terms
+  end
+
+  def privacy
+  end
+
+  private
+
+  def set_application_name
+    # To modify, change the module/app name in config/application.rb or
+    # hard-code this value to a string
+    @app_name = Rails.application.class.module_parent_name.to_s
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root "pages#splash"
+  get "/privacy", to: "pages#privacy"
+  get "/terms", to: "pages#terms"
 end
